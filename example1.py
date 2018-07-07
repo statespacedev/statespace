7d3a37261ff4b51cd057c3876749c85baca4aeb2
Noah Smith <noahhsmith@gmail.com>
2018-07-07 09:50:20 +0200

    sync

---
+++ example1.py
@@ -0,0 +1,6 @@
+# linearized bayesian processor, lz-bp, linear kalman filter
+import numpy as np
+
+t = np.arange(0, 1.5, .01)
+
+pass
--- lz-bp.py
+++
@@ -1,6 +0,0 @@
-# linearized bayesian processor
-import numpy as np
-
-t = np.arange(0, 1.5, .01)
-
-pass
