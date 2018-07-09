e6cab33635e70c5c52c242c9e3c7436f74ae3820
Noah Smith <smith@vesseltracker.com>
2018-07-09 09:57:27 +0200

    sync

--- case1.py
+++ case1.py
@@ -19,8 +19,8 @@ def C(x):
 
 tts = np.arange(0, n * deltat, deltat)
 Rvv = .09**2
-vts = math.sqrt(Rvv) * np.random.randn(n)
 Rww = 0
+vts = math.sqrt(Rvv) * np.random.randn(n)
 wts = math.sqrt(Rww) * np.random.randn(n)
 
 xts = np.zeros_like(tts)
@@ -31,40 +31,43 @@ Ptilts = np.zeros_like(tts)
 Reets = np.zeros_like(tts)
 Kts = np.zeros_like(tts)
 yhatts = np.zeros_like(tts)
-ets = np.zeros_like(tts)
+ytillts = np.zeros_like(tts)
 xtilts = np.zeros_like(tts)
 
 xts[0] = 2. + wts[0]
 yts[0] = y(xts[0], vts[0])
 xrefts[0] = 2.
-xhatts[0] = 2.3
+xhatts[0] = 2.0
 Ptilts[0] = .01
 Reets[0] = 0
 Kts[0] = 0
 yhatts[0] = y(xrefts[0], 0) + C(xrefts[0]) * (xhatts[0] - xrefts[0])
-ets[0] = 0
+ytillts[0] = 0
 xtilts[0] = xhatts[0] - xts[0]
 
 for tk in range(1, n):
     xts[tk] = x(xts[tk - 1], wts[tk - 1])
     yts[tk] = y(xts[tk], vts[tk])
-
     xrefts[tk] = 2. + .067 * tts[tk]
-    xhatts[tk] = (1 - .05 * deltat) * xrefts[tk-1] + A(xrefts[tk-1]) * (xhatts[tk-1] - xrefts[tk-1])
+
+    # xhatts[tk] = (1 - .05 * deltat) * xrefts[tk-1] + A(xrefts[tk-1]) * (xhatts[tk-1] - xrefts[tk-1])
+    # xhatts[tk] = xrefts[tk] + A(xrefts[tk-1]) * (xhatts[tk-1] - xrefts[tk-1])
+    xhatts[tk] = x(xrefts[tk-1], 0) + A(xrefts[tk-1]) * (xhatts[tk-1] - xrefts[tk-1])
     Ptilts[tk] = A(xrefts[tk-1])**2 * Ptilts[tk-1]
 
     Reets[tk] = C(xhatts[tk])**2 * Ptilts[tk] + .09
     Kts[tk] = util.div0( Ptilts[tk] * C(xrefts[tk]) , Reets[tk] )
 
     yhatts[tk] = y(xrefts[tk], 0) + C(xrefts[tk]) * (xhatts[tk] - xrefts[tk])
-    ets[tk] =  yts[tk] - yhatts[tk]
+    ytillts[tk] = yts[tk] - yhatts[tk]
 
-    xhatts[tk] = xhatts[tk] + Kts[tk] * ets[tk]
+    xhatts[tk] = xhatts[tk] + Kts[tk] * ytillts[tk]
     Ptilts[tk] = (1 - Kts[tk] * C(xrefts[tk])) * Ptilts[tk]
 
     xtilts[tk] = xhatts[tk] - xts[tk]
 
-plots.xts(ets, tts, end=None)
+# plots.xts(xhatts, tts, end=None)
+plots.test(xhatts, xtilts, yhatts, ytillts, yts, Reets, tts)
 
 pass
 
--- plots.py
+++ plots.py
@@ -8,3 +8,19 @@ def xts(xts, tts, end=None):
     plt.plot(tts[:x2], xts[:x2])
     plt.show()
     pass
+
+def test(xhatts, xtilts, yhatts, ytillts, yts, Reets, tts):
+    lw = 1
+    plt.subplot(3,2,1)
+    plt.plot(tts, xhatts, linewidth=lw)
+    plt.subplot(3,2,2)
+    plt.plot(tts, xtilts, linewidth=lw)
+    plt.subplot(3,2,3)
+    plt.plot(tts, yhatts, linewidth=lw)
+    plt.subplot(3,2,4)
+    plt.plot(tts, ytillts, linewidth=lw)
+    plt.subplot(3,2,5)
+    plt.plot(tts, yts, linewidth=lw)
+    plt.subplot(3,2,6)
+    plt.plot(tts, Reets, linewidth=lw)
+    plt.show()
