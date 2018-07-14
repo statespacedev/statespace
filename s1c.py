c57695b0376f96873164cc24dd323bb32f8321bb
Noah Smith <noahhsmith@gmail.com>
2018-07-15 00:05:35 +0200

    sync

--- s1c.py
+++ s1c.py
@@ -19,42 +19,68 @@ def c(x):
 
 Rvv = .09
 Rww = 0
-bignsubx = 1
-kappa = 1
 tts = np.arange(0, n * deltat, deltat)
 vts = math.sqrt(Rvv) * np.random.randn(n)
 wts = math.sqrt(Rww) * np.random.randn(n)
 
-xts = np.zeros_like(tts)
-yts = np.zeros_like(tts)
-xhatts = np.zeros_like(tts)
-Ptilts = np.zeros_like(tts)
-Reets = np.zeros_like(tts)
-Kts = np.zeros_like(tts)
-yhatts = np.zeros_like(tts)
-ets = np.zeros_like(tts)
-xtilts = np.zeros_like(tts)
-W = np.zeros((3,))
-Xts = np.zeros((n, 3))
-Xtilts = np.zeros((n, 3))
-Xhatts = np.zeros((n, 3))
-Yts = np.zeros((n, 3))
-ksits = np.zeros((n, 3))
-
+xts = np.zeros((n,))
+yts = np.zeros((n,))
 xts[0] = 2.
 yts[0] = y(xts[0], vts[0])
-xhatts[0] = 2.
-Ptilts[0] = .01
-Reets[0] = 0
-Kts[0] = 0
-ets[0] = 0
-xtilts[0] = xhatts[0] - xts[0]
-W[:] = [kappa / float(bignsubx + kappa), .5 * kappa / float(bignsubx + kappa), .5 * kappa / float(bignsubx + kappa)]
-Xts[0, :] = [xhatts[0], xhatts[0] + math.sqrt((bignsubx + kappa) * Ptilts[0]), xhatts[0] - math.sqrt((bignsubx + kappa) * Ptilts[0])]
-Xtilts[0, :] = [Xts[0, 0] - xhatts[0], Xts[0, 1] - xhatts[0], Xts[0, 2] - xhatts[0]]
-Xhatts[0, :] = Xts[0, :]
+
+xhat0 = 2.1
+Ptil0 = .01
+
+bignsubx = 1
+kappa = 1
+bkfac = kappa / float(bignsubx + kappa)
+W = np.zeros((3,))
+W[:] = [bkfac, bkfac / 2., bkfac / 2.]
+
+def X(xhat, Ptil):
+    a = math.sqrt((bignsubx + kappa) * Ptil)
+    return [xhat, xhat + a, xhat - a]
+Xts = np.zeros((n, 3))
+Xts[0, :] = X(xhat0, Ptil0) # Xts[1, :] = a(Xts[0, :]) + b(u)
+
+xhatts = np.zeros((n,))
+xhatts[0] = xhat0 # xhatts[1] = W @ Xts[1, :]
+
+Xtilts = np.zeros((n, 3))
+Xtilts[0, :] = Xts[0, :] - xhatts[0]
+
+Ptilts = np.zeros((n,))
+Ptilts[0] = Ptil0 # Ptilts[1, :] = W @ Xtilts[1, :] * np.ones((3,)) @ Xtilts[1, :] + Rww
+
+def Xhat(X, Rww):
+    a = kappa * math.sqrt(Rww)
+    return [X, X + a, X - a]
+Xhatts = np.zeros((n, 3))
+Xhatts[0, :] = Xts[0, :] # Xhatts[1, :] = Xhat(Xts[1, :], Rww)
+
+Yts = np.zeros((n, 3))
 Yts[0, :] = c(Xhatts[0, :])
+
+yhatts = np.zeros((n,))
 yhatts[0] = W @ Yts[0, :]
+
+ksits = np.zeros((n, 3))
+ksits[0, :] = Yts[0, :] - yhatts[0]
+
+Rksiksi = np.zeros((n,))
+Rksiksi[0] = W @ ksits[0, :] * np.ones((3,)) @ ksits[0, :] + Rvv
+
+RXtilksi = np.zeros((n,))
+RXtilksi[0] = W @ Xtilts[0, :] * np.ones((3,)) @ ksits[0,:]
+
+Kts = np.zeros((n,))
+Kts[0] = RXtilksi[0] / Rksiksi[0]
+
+ets = np.zeros((n,))
+ets[0] = yts[0] - yhatts[0]
+
+xtilts = np.zeros((n,))
+xtilts[0] = xhatts[0] - xts[0]
 
 for tk in range(1, n):
     xts[tk] = x(xts[tk - 1], wts[tk - 1])
