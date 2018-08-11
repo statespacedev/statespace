2dd4057aada1b687926e239367c5f200659c4485
Noah Smith <noahhsmith@gmail.com>
2018-08-11 10:36:26 +0200

    sync

--- jazwinski-pf.py
+++ jazwinski-pf.py
@@ -6,7 +6,6 @@ nsamp = 250
 n = 150
 deltat = .01
 
-
 tts = np.arange(0, n * deltat, deltat)
 Rww = 1e-6
 wts = math.sqrt(Rww) * np.random.randn(n)
@@ -15,40 +14,32 @@ vts = math.sqrt(Rvv) * np.random.randn(n)
 
 def fx(x, w):
     return (1 - .05 * deltat) * x + .04 * deltat * x**2 + w
-
-vfA = np.vectorize(fx)
-
 def fy(x, v):
     return x ** 2 + x ** 3 + v
 
-def fc(y, xi):
-    return -.5 * math.log(2 * math.pi * Rvv) - (y - xi**2 - xi**3)**2 / (2 * Rvv)
-
-vfC = np.vectorize(fc)
-
 xts = np.zeros((n,))
 xts[0] = 2.
+P0 = 1e-20
 yts = np.zeros((n,))
 yts[0] = fy(xts[0], vts[0])
 for tk in range(1, n):
     xts[tk] = fx(xts[tk - 1], wts[tk - 1])
     yts[tk] = fy(xts[tk], vts[tk])
 
-wits = math.sqrt(Rww) * np.random.randn(n, nsamp)
-xits = np.zeros((n, nsamp))
-xits[0, :] = xts[0] + math.sqrt(1e-20) * np.random.randn(nsamp)
+def fA(xi, wi):
+    return (1 - .05 * deltat) * xi + .04 * deltat * xi**2 + wi
+vfA = np.vectorize(fA)
+def fC(y, xi):
+    return np.exp(-.5 * np.log(2 * np.pi * Rvv) - (y - xi**2 - xi**3)**2 / (2 * Rvv))
+vfC = np.vectorize(fC)
 
+xits = np.zeros((n, nsamp))
+xits[0, :] = xts[0] + math.sqrt(P0) * np.random.randn(nsamp)
 Wits = np.zeros((n, nsamp))
 
 for tk in range(1, n):
-    xts[tk] = fx(xts[tk - 1], wts[tk - 1])
-    yts[tk] = fy(xts[tk], vts[tk])
-
-    xits[tk, :] = vfA(xits[tk - 1, :], wits[tk - 1, :])
-
+    xits[tk, :] = vfA(xits[tk - 1, :], math.sqrt(Rww) * np.random.randn(nsamp))
     Wi = vfC(yts[tk], xits[tk, :])
     Wits[tk, :] = Wi / sum(Wi)
-
-
 
 pass
