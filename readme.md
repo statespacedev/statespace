d1237e26260355ca5a5c70b30308bd1ddf113ba0
Noah Smith <smith@vesseltracker.com>
2018-08-17 12:10:48 +0200

    sync

--- readme.md
+++ readme.md
@@ -15,10 +15,11 @@ risk and the cost of errors
     optional arguments:
       -h, --help  show this help message and exit
       -t          test the project
-      -lzkf       linearized processor
-      -ekf        extended processor
-      -ukf        sigma-point sampling processor
-      -pf         sequential monte carlo processor
+      -lzkf       linearized processor, linearized kalman filter
+      -ekf        extended processor, extended kalman filter
+      -ukf        sigma-point sampling processor, unscented kalman filter
+      -pf         sequential monte carlo processor, particle filter
+
 
 ## background
 
--- statespace/__main__.py
+++ statespace/__main__.py
@@ -5,16 +5,15 @@ import class_statespace
 def main():
     parser = argparse.ArgumentParser('statespace')
     parser.add_argument('-t', help='test the project', action='store_true')
-    parser.add_argument('-lzkf', help='linearized processor', action='store_true')
-    parser.add_argument('-ekf', help='extended processor', action='store_true')
-    parser.add_argument('-ukf', help='sigma-point sampling processor', action='store_true')
-    parser.add_argument('-pf', help='sequential monte carlo processor', action='store_true')
+    parser.add_argument('-lzkf', help='linearized processor, linearized kalman filter', action='store_true')
+    parser.add_argument('-ekf', help='extended processor, extended kalman filter', action='store_true')
+    parser.add_argument('-ukf', help='sigma-point sampling processor, unscented kalman filter', action='store_true')
+    parser.add_argument('-pf', help='sequential monte carlo processor, particle filter', action='store_true')
     args = parser.parse_args()
 
     ss = class_statespace.Statespace()
     if args.t:
         print('test successful')
-
     if args.lzkf:
         ss.lzkf()
     if args.ekf:
