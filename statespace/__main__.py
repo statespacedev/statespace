dc091a6c9c7ff5847919268ffeb8192536c70474
Noah Smith <noahhsmith@gmail.com>
2018-08-26 11:32:49 +0200

    sync

--- statespace/__main__.py
+++ statespace/__main__.py
@@ -3,31 +3,32 @@ sys.path.append('statespace')
 
 def main():
     parser = argparse.ArgumentParser('statespace')
-    parser.add_argument('-t', '--test', help='test the project', action='store_true')
-    parser.add_argument('-l', '--lzkf', help='linearized processor, linearized kalman filter', action='store_true')
-    parser.add_argument('-e', '--ekf', help='extended processor, extended kalman filter', action='store_true')
-    parser.add_argument('-u', '--ukf', help='sigma-point sampling processor, unscented kalman filter', action='store_true')
-    parser.add_argument('-p', '--pf', help='sequential monte carlo processor, particle filter', action='store_true')
+    parser.add_argument('--lzbp', help='linearized bayesian processor, linearized kalman filter', action='store_true')
+    parser.add_argument('--xbp', help='extended bayesian processor, extended kalman filter', action='store_true')
+    parser.add_argument('--spbp', help='sigma-point bayesian processor, unscented kalman filter', action='store_true')
+    parser.add_argument('--sspf', help='state space particle filter', action='store_true')
+    parser.add_argument('--jbp', help='joint bayesian state/parameteric processors', action='store_true')
     args = parser.parse_args()
 
-    if args.test:
-        print('test successful')
+    if args.lzbp:
+        import bsp1_lzbp
+        bsp1_lzbp.main()
 
-    if args.lzkf:
-        import lzkf
-        lzkf.main()
+    if args.xbp:
+        import bsp1_xbp
+        bsp1_xbp.main()
 
-    if args.ekf:
-        import ekf
-        ekf.main()
+    if args.spbp:
+        import bsp1_spbp
+        bsp1_spbp.main()
 
-    if args.ukf:
-        import ukf
-        ukf.main()
+    if args.sspf:
+        import bsp1_sspf
+        bsp1_sspf.main()
 
-    if args.pf:
-        import pf
-        pf.main()
+    if args.jbp:
+        import bsp1_jbp
+        bsp1_jbp.main()
 
 if __name__ == "__main__":
     main()
