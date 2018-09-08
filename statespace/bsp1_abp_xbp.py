3c39129c28fb63ee0a3333dfe16ac209c416a155
Noah Smith <noahhsmith@gmail.com>
2018-09-08 20:50:04 +0200

    sync

--- statespace/bsp1_abp_xbp.py
+++ statespace/bsp1_abp_xbp.py
@@ -50,39 +50,68 @@ C = fC(xhatts[tk, :])
 Reets = np.zeros(n)
 Reets[tk] = C @ Ptilts[tk, :, :] @ C + Rvv
 
-def ver1():
+def ver1(tk):
     xhatts[tk, :] = fx(xhatts[tk-1, :], 0)
     Ptilts[tk, :, :] = fA(xhatts[tk-1, :]) @ Ptilts[tk-1, :, :] @ fA(xhatts[tk-1, :]).T + Rww
     yhatts[tk] = fy(xhatts[tk, :], 0)
     ets[tk] = yts[tk] - yhatts[tk]
     C = fC(xhatts[tk, :])
-    Reets[tk] = C @ Ptilts[tk, :, :] @ C + Rvv
-    K = Ptilts[tk, :, :] @ C / Reets[tk]
+    Reets[tk] = C @ Ptilts[tk, :, :] @ C.T + Rvv
+    K = Ptilts[tk, :, :] @ C.T / Reets[tk]
     xhatts[tk, :] = xhatts[tk, :] + K * ets[tk]
     Ptilts[tk, :, :] = (np.eye(3) - K @ C) @ Ptilts[tk, :, :]
     xtilts[tk, :] = xts[tk, :] - xhatts[tk, :]
 
-def fUD(P):
-    P, L, U = la.lu(P)
-    D = np.diag(np.diag(U))   # D is just the diagonal of U
+def ver2(tk):
+    xhatts[tk, :] = fx(xhatts[tk-1, :], 0)
+    P = Ptilts[tk-1, :, :]
+    #P = fA(xhatts[tk-1, :]) @ P @ fA(xhatts[tk-1, :]).T + Rww
+    yhatts[tk] = fy(xhatts[tk, :], 0)
+    ets[tk] = yts[tk] - yhatts[tk]
+    C = fC(xhatts[tk, :])
+    z = yhatts[tk]
+    R = Rvv
+    H = C
+    tmp, L, U = la.lu(P)
+    D = np.diag(np.diag(U))
     U /= np.diag(U)[:, None]
-    return U, D
-
-def ver2():
-    xhatts[tk, :] = fx(xhatts[tk-1, :], 0)
-    Ptilts[tk, :, :] = fA(xhatts[tk-1, :]) @ Ptilts[tk-1, :, :] @ fA(xhatts[tk-1, :]).T + Rww
-    U, D = fUD(Ptilts[tk, :, :])
-    yhatts[tk] = fy(xhatts[tk, :], 0)
-    ets[tk] = yts[tk] - yhatts[tk]
-    C = fC(xhatts[tk, :])
-    Reets[tk] = C @ Ptilts[tk, :, :] @ C + Rvv
-    K = Ptilts[tk, :, :] @ C / Reets[tk]
-    xhatts[tk, :] = xhatts[tk, :] + K * ets[tk]
-    Ptilts[tk, :, :] = (np.eye(3) - K @ C) @ Ptilts[tk, :, :]
-    xtilts[tk, :] = xts[tk, :] - xhatts[tk, :]
+    x = xhatts[tk, :]
+    v = np.zeros(3)
+    w = np.zeros(3)
+    delta = z
+    for j in range(3):
+        delta = delta - H[j] * x[j]
+        v[j] = H[j]
+        jlim = j - 1
+        if jlim < 0:
+            continue
+        for i in range(jlim+1):
+            v[j] = v[j] + U[i, j] * H[i]
+    sigma = R
+    for j in range(3):
+        nu = v[j]
+        v[j] = v[j] * D[j, j]
+        w[j] = nu
+        jlim = j - 1
+        if jlim < 0:
+            continue
+        for i in range(jlim+1):
+            tau = U[i, j] * nu
+            U[i, j] = U[i, j] - nu * w[i] / sigma
+            w[i] = w[i] + tau
+        D[j, j] = D[j, j] * sigma
+        sigma = sigma + nu * v[j]
+        D[j, j] = D[j, j] * sigma
+    epsilon = delta / sigma
+    for i in range(3):
+        x[i] = x[i] + v[i] * epsilon
+    xhatts[tk, :] = x
+    P = U @ D @ U.T
+    Ptilts[tk, :, :] = P
+    pass
 
 for tk in range(1, n):
-    ver2()
+    ver2(tk)
 
 innov = class_residuals.Residuals(tts, ets)
 innov.abp(tts, xhatts, xtilts, yhatts)
