893d9cb04848b0c88fec6b5ebc629ce3c487ff88
Noah Smith <smith@vesseltracker.com>
2018-12-16 16:48:13 +0100

    sync

--- setup.py
+++ setup.py
@@ -6,14 +6,31 @@ if os.environ.get('CI_COMMIT_TAG'):
 elif os.environ.get('CI_JOB_ID'):
     version = os.environ['CI_JOB_ID']
 
-setuptools.setup(
-    name='statespace',
-    version=version,
-    author='noah smith',
-    author_email='noahhsmith@gmail.com',
-    description='state-space distributions and decisions',
-    url='https://gitlab.com/noahhsmith/statespace',
-    packages=setuptools.find_packages(),
-    entry_points={'console_scripts': ['statespace = statespace.__main__:main']},
-    zip_safe=False,
-)
+with open('requirements.txt') as fin:
+    required = fin.read().splitlines()
+
+if version:
+    setuptools.setup(
+        name='statespace',
+        version=version,
+        author='noah smith',
+        author_email='noahhsmith@gmail.com',
+        description='state-space distributions and decisions',
+        url='https://gitlab.com/noahhsmith/statespace',
+        packages=setuptools.find_packages(),
+        entry_points={'console_scripts': ['statespace = statespace.__main__:main']},
+        install_requires=required,
+        zip_safe=False,
+    )
+else:
+    setuptools.setup(
+        name='statespace',
+        author='noah smith',
+        author_email='noahhsmith@gmail.com',
+        description='state-space distributions and decisions',
+        url='https://gitlab.com/noahhsmith/statespace',
+        packages=setuptools.find_packages(),
+        entry_points={'console_scripts': ['statespace = statespace.__main__:main']},
+        install_requires=required,
+        zip_safe=False,
+    )
