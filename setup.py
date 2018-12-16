999ebe1b78dd005b00ecab58b8d2a64e25485cca
Noah Smith <smith@vesseltracker.com>
2018-12-16 01:44:37 +0100

    sync

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-import setuptools
+import setuptools, os
+
+if os.environ.get('CI_COMMIT_TAG'):
+    version = os.environ['CI_COMMIT_TAG']
+else:
+    version = os.environ['CI_JOB_ID']
 
 with open('readme.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='statespace',
-    version='2018.3.1',
+    version=version,
     author='noah smith',
     author_email='noahhsmith@gmail.com',
     description='state-space distributions and decisions',
@@ -13,14 +18,6 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://gitlab.com/noahhsmith/statespace',
     packages=setuptools.find_packages(),
-    entry_points={
-        'console_scripts': [
-            'statespace = statespace.__main__:main'
-        ]
-    },
-    classifiers=(
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ),
+    entry_points={'console_scripts': ['statespace = statespace.__main__:main']},
+    zip_safe=False,
 )
