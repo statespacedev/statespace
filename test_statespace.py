3e1ea6b3ed7676f32af9b1e64ae744f73dc93af6
Noah Smith <smith@vesseltracker.com>
2018-12-26 08:38:49 +0100

    sync

--- test_statespace.py
+++ test_statespace.py
@@ -1,34 +1,34 @@
 import sys
 sys.path.append('statespace')
 
-lo, hi = 2.06, 2.14
+tol1 = 1.
 
 from statespace import classical
 
 def test_classical_001():
     tmp = classical.Classical('linearized', plot=False)
-    assert(tmp.log[-1][1] > lo and tmp.log[-1][1] < hi)
+    assert(abs(tmp.log[-1][3]) < tol1)
 
 def test_classical_002():
     tmp = classical.Classical('extended', plot=False)
-    assert(tmp.log[-1][1] > lo and tmp.log[-1][1] < hi)
+    assert(abs(tmp.log[-1][3]) < tol1)
 
 def test_classical_003():
     tmp = classical.Classical('adaptive', plot=False)
-    assert(tmp.log[-1][1] > lo and tmp.log[-1][1] < hi)
+    assert(abs(tmp.log[-1][3]) < tol1)
 
 from statespace import modern
 
 def test_modern_001():
     tmp = modern.Modern('sigmapoint', plot=False)
-    assert(tmp.log[-1][1] > lo and tmp.log[-1][1] < hi)
+    assert(abs(tmp.log[-1][3]) < tol1)
 
 def test_modern_002():
     tmp = modern.Modern('sigmapoint2', plot=False)
-    assert(tmp.log[-1][1] > lo and tmp.log[-1][1] < hi)
+    assert(abs(tmp.log[-1][3]) < tol1)
 
 from statespace import particle
 
 def test_particle_001():
     tmp = particle.Particle('bootstrap', plot=False)
-    assert(tmp.log[-1][1] > lo and tmp.log[-1][1] < hi)
+    assert(abs(tmp.log[-1][3]) < tol1)
