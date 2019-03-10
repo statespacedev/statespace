fd50b110ee9bc25da3609ab8ff7211ce4eaa29b5
Noah Smith <smith@vesseltracker.com>
2019-03-10 10:54:12 +0100

    sync

--- statespace/decisions.py
+++ statespace/decisions.py
@@ -3,12 +3,40 @@ import matplotlib.pyplot as plt
 
 
 class DecisionsEns():
-    def __init__(self, title):
-        self.title = title
-        self.decisionfunctions = []
+    def __init__(self):
+        self.decisionfunctionssig = []
+        self.decisionfunctionsnoise = []
+        self.rocs = []
 
-    def update(self, decisionfunction):
-        self.decisionfunctions.append(decisionfunction)
+    def addsig(self, decisionfunction):
+        self.decisionfunctionssig.append(decisionfunction)
+
+    def addnoise(self, decisionfunction):
+        self.decisionfunctionsnoise.append(decisionfunction)
+
+    def plot_decisionfunctions(self):
+        from pymc3.plots.kdeplot import fast_kde
+        import matplotlib.pyplot as plt
+        globalmin = np.floor(np.min(self.decisionfunctionsnoise))
+        globalmax = np.ceil(np.max(self.decisionfunctionssig))
+        def pmf(vals):
+            vals = np.append(vals, [globalmin, globalmax])
+            pmf, min, max = fast_kde(vals, bw=4.5)
+            x = np.linspace(min, max, len(pmf))
+            pmf = pmf / sum(pmf)
+            return x, pmf
+        plt.figure()
+        for ndx in range(len(self.decisionfunctionssig)):
+            sigx, sigpmf = pmf(self.decisionfunctionssig[ndx])
+            noisex, noisepmf = pmf(self.decisionfunctionsnoise[ndx])
+            plt.plot(sigx, sigpmf, 'g', alpha=.1)
+            plt.plot(noisex, noisepmf, 'b', alpha=.1)
+        sigx, sigpmf = pmf(self.decisionfunctionssig)
+        noisex, noisepmf = pmf(self.decisionfunctionsnoise)
+        plt.plot(sigx, sigpmf, 'g')
+        plt.plot(noisex, noisepmf, 'b')
+        plt.xlabel('decision function output-value')
+        plt.ylabel('p')
 
 class Decisions():
     def __init__(self, mode, tracker):
@@ -28,40 +56,20 @@ class Decisions():
             else: decisionfunction[ndx] = decisionfunction[ndx-1] + delta
         return decisionfunction
 
-def rccircuit(runs, signal, title=None):
+def rccircuit(runs):
     from classical import Classical
-    ens = DecisionsEns(title)
+    ens = DecisionsEns()
     for runndx in range(runs):
-        tracker = Classical(mode='rccircuit', plot=False, signal=signal)
+        tracker = Classical(mode='rccircuit', plot=False, signal=300.)
         dec = Decisions(mode='rccircuit', tracker=tracker)
-        ens.update(decisionfunction=dec.decisionfunction)
+        ens.addsig(decisionfunction=dec.decisionfunction)
+        tracker = Classical(mode='rccircuit', plot=False, signal=0.)
+        dec = Decisions(mode='rccircuit', tracker=tracker)
+        ens.addnoise(decisionfunction=dec.decisionfunction)
     return ens
 
-def plot_ensemble_decision_function(enssig, ensnoise):
-    def pmf(vals):
-        pmf, min, max = fast_kde(vals, bw=4.5)
-        x = np.linspace(min, max, len(pmf))
-        pmf = pmf / sum(pmf)
-        return x, pmf
-    from pymc3.plots.kdeplot import fast_kde
-    import matplotlib.pyplot as plt
-    plt.figure()
-    for ndx in range(len(enssig.decisionfunctions)):
-        sigx, sigpmf = pmf(enssig.decisionfunctions[ndx])
-        noisex, noisepmf = pmf(ensnoise.decisionfunctions[ndx])
-        plt.plot(sigx, sigpmf, 'g', alpha=.1)
-        plt.plot(noisex, noisepmf, 'b', alpha=.1)
-    sigx, sigpmf = pmf(enssig.decisionfunctions)
-    noisex, noisepmf = pmf(ensnoise.decisionfunctions)
-    plt.plot(sigx, sigpmf, 'g')
-    plt.plot(noisex, noisepmf, 'b')
-    plt.xlabel('decision function value')
-    plt.ylabel('p')
-
 if __name__ == "__main__":
-    enssig = rccircuit(runs=100, signal=300., title='signal')
-    ensnoise = rccircuit(runs=100, signal=0., title='noise')
-    plot_ensemble_decision_function(enssig=enssig, ensnoise=ensnoise)
+    ens = rccircuit(runs=100)
+    ens.plot_decisionfunctions()
     plt.show()
-
     pass
