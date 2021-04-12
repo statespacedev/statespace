30d395e20ad65847bb428a01ae7725cefdd33c0f
Noah Smith <smith@vesseltracker.com>
2021-04-12 06:06:20 +0200

    sync

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ from models.bearingsonly import BearingsOnly
 cases ={
     '1a': {'model': Onestate(), 'processor': Kalman()},
     '1b': {'model': Threestate(), 'processor': Kalman()},
-    '1c': {'model': BearingsOnly(), 'processor': Kalman()},
-    '2a': {'model': Onestate(), 'processor': Kalman('ud')},
-    '2b': {'model': Threestate(), 'processor': Kalman('ud')},
-    '3a': {'model': Onestate(), 'processor': SigmaPoint()},
-    '3b': {'model': Threestate(), 'processor': SigmaPoint()},
-    '3c': {'model': BearingsOnly(), 'processor': SigmaPoint()}, # tuning
-    '4a': {'model': Onestate(), 'processor': SigmaPoint('cholesky')},
-    '4b': {'model': Threestate(), 'processor': SigmaPoint('cholesky')},
-    '5a': {'model': Onestate(), 'processor': Particle()},
-    '5b': {'model': Threestate(), 'processor': Particle()},
-    '5c': {'model': BearingsOnly(), 'processor': Particle()}, # tuning
+    '1c': {'model': Onestate(), 'processor': Kalman('ud')},
+    '1d': {'model': Threestate(), 'processor': Kalman('ud')},
+    '2a': {'model': Onestate(), 'processor': SigmaPoint()},
+    '2b': {'model': Threestate(), 'processor': SigmaPoint()},
+    '2c': {'model': Onestate(), 'processor': SigmaPoint('cholesky')},
+    '2d': {'model': Threestate(), 'processor': SigmaPoint('cholesky')},
+    '3a': {'model': Onestate(), 'processor': Particle()},
+    '3b': {'model': Threestate(), 'processor': Particle()},
+    'bo1': {'model': BearingsOnly(), 'processor': Kalman()},
+    'bo2': {'model': BearingsOnly(), 'processor': SigmaPoint()}, # tuning
+    'bo3': {'model': BearingsOnly(), 'processor': Particle()}, # tuning
 }
-case = '5c'
+case = 'bo1'
 
 def main():
     model, processor = cases[case]['model'], cases[case]['processor']
