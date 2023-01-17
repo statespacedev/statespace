a3009d3c04a4048d3ac1bddf0f99a1c4e294fa3d
Noah Smith <nsmith>
2023-01-17 19:45:31 +0100

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,10 +13,10 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("./statespace"))
-sys.path.insert(0, os.path.abspath("./statespace/models"))
-sys.path.insert(0, os.path.abspath("./statespace/processors"))
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../statespace"))
+sys.path.insert(0, os.path.abspath("../statespace/models"))
+sys.path.insert(0, os.path.abspath("../statespace/processors"))
 
 
 # -- Project information -----------------------------------------------------
