d8442dc8429ad4e84ff2543eb330133e3dc3f76c
Noah Smith <nsmith>
2023-01-17 19:54:50 +0100

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,8 @@
 import os
 import sys
 
-# sys.path.insert(0, os.path.abspath(".."))
-# sys.path.insert(0, os.path.abspath("../statespace"))
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../statespace"))
 # sys.path.insert(0, os.path.abspath("../statespace/models"))
 # sys.path.insert(0, os.path.abspath("../statespace/processors"))
 
